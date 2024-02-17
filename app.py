38031049d16bda20f138bea9396033e080879dd7
Nicolas Castellano <nicolas.castellano.6@hotmail.com>
2024-02-16 20:55:56 -0400

    .

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv, find_dotenv
 import os
 
 # Load environment variables from .env file
-load_dotenv(find_dotenv())
+load_dotenv()
 
 # initialize the Flask app
 app = Flask(__name__)
